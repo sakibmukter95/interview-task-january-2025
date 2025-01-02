383b73f0e8342b7cd52a9c29c581cf6b0ecc151f
Sakib Mukter <sakibmukter77@gmail.com>
2025-01-02 14:33:34 +0100

    add global error handler middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import deviceRoutes from "./routes/deviceRoutes";
+import bodyParser from "body-parser";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 
+// Middleware
+app.use(bodyParser.json());
+
+//Routes
 app.use("/api/devices", deviceRoutes);
+
+// Global Error Handler
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`[backend]: Server is running at port: ${port}`);
